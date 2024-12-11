840210fbd70f791299d6220e3bc4d1902f8a5153
Daan Speth <daanspeth@gmail.com>
2024-12-11 11:27:34 +0100

    barebones command line interface setup

---
+++ __main__.py
@@ -0,0 +1,52 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+import sys
+import traceback
+
+from aastk import __author__, __copyright__, __version__
+from metacooc.cli import parse_cli
+
+def print_help():
+    print('''\
+
+  MetaCoOc v%s
+
+  Tools:
+    setup -> Protein Alignment Score Ratio calculation
+
+  Use: metacooc <command> -h for command specific help
+    ''' % __version__)
+
+
+def main():
+    args = None
+    if len(sys.argv) == 1:
+        print_help()
+        sys.exit(0)
+    elif sys.argv[1] in {'-v', '--v', '-version', '--version'}:
+        print(f"aastk: version {__version__} {__copyright__} {__author__}")
+        sys.exit(0)
+    elif sys.argv[1] in {'-h', '--h', '-help', '--help'}:
+        print_help()
+        sys.exit(0)
+    else:
+        parse_cli()
+
+
+if __name__ == "__main__":
+    main()
---
+++ metacooc/__init__.py
@@ -0,0 +1,31 @@
+###############################################################################
+#                                                                             #
+#    This program is free software: you can redistribute it and/or modify     #
+#    it under the terms of the GNU General Public License as published by     #
+#    the Free Software Foundation, either version 3 of the License, or        #
+#    (at your option) any later version.                                      #
+#                                                                             #
+#    This program is distributed in the hope that it will be useful,          #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of           #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            #
+#    GNU General Public License for more details.                             #
+#                                                                             #
+#    You should have received a copy of the GNU General Public License        #
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+
+__author__ = 'Ben Coltman'
+__author_email__ = 'ben.coltman@univie.ac.at'
+__copyright__ = 'Copyright 2024'
+__credits__ = ['Ben Coltman, Daan Speth']
+__description__ = 'Co-occurence patterns from microbial metagenomes'
+__license__ = 'GPL3'
+__maintainer__ = 'Ben Coltman, Daan Speth'
+__maintainer_email__ = 'daan.speth@univie.ac.at'
+__name__ = 'metacooc'
+__python_requires__ = '>=3.8'
+__status__ = 'development'
+__title__ = 'MetaCoOc'
+__url__ = 'https://github.com/bcoltman/metacooccur'
+__version__ = '0.0.1'
