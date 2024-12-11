af4ed773709b0e19ddef2d43104c2f5f7b71d35f
Daan Speth <daanspeth@gmail.com>
2024-12-11 12:24:41 +0100

    add planned functionality to cli file

--- metacooc/cli.py
+++ metacooc/cli.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python3
 
-## placeholder for now
 
 import argparse
-from aastk.pasr import extract_matching_sequences
+# from metacooc.filter import xyz
 
 def parse_cli():
-    parser = argparse.ArgumentParser(description="Amino Acid Sequence Toolkit")
+    parser = argparse.ArgumentParser(description="Co-occurence data of microorganims based on metagenome detection")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
-    # Subcommand for 'pasr'
-    pasr_parser = subparsers.add_parser("pasr", help="PASR: protein alignment score ratio")
-    pasr_parser.add_argument("blast_tab", help="BLAST/DIAMOND tabular output file")
-    pasr_parser.add_argument("seq_file", help="FASTA or FASTQ file")
-    pasr_parser.add_argument("out_fasta", help="Output file for matched sequences")
+    # Subcommand for 'setup'
+    setup_parser = subparsers.add_parser("setup", help="download and set up required data co-occurence analysis")
+# example line to add arguments
+#   pasr_parser.add_argument("blast_tab", help="BLAST/DIAMOND tabular output file")
+
+    # Subcommand for 'cooccurrence'
+    cooc_parser = subparsers.add_parser("cooccurrence", help="run the full co-occurence workflow")
+
+    # Subcommand for 'filter'
+    filter_parser = subparsers.add_parser("filter", help="filter data by search term or list of accession numbers")
+
+    # Subcommand for 'ratio'
+    ratio_parser = subparsers.add_parser("ratio", help="calculate cooccurrence ratios for all taxa in the filtered set")
+
+    # Subcommand for 'search'
+    search_parser = subparsers.add_parser("search", help="search metadata and taxonomy files for the occurrence of terms")
+
+    # Subcommand for 'list'
+    list_parser = subparsers.add_parser("list", help="get a list of accession numbers based on filtering criteria")
+
 
     args = parser.parse_args()
 
-    if args.command == "pasr":
-        extract_matching_sequences(args.blast_tab, args.seq_file, args.out_fasta)
+    if args.command == "setup":
+        # do stuff
+    elif args.command == "cooccurrence":
+        # do stuff
+    elif args.command == "filter":
+        # do stuff
+    elif args.command == "ratio":
+        # do stuff
+    elif args.command == "search":
+        # do stuff
+    elif args.command == "list":
+        # do stuff
+
+
+
+
+
